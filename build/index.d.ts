784025ce98680cf9a8baa59123ab8bbcfebcbb3d
toyjhlee <toyjhlee@users.noreply.github.com>
2021-03-18 18:00:39 +0900

    Delete build directory

--- build/index.d.ts
+++
@@ -1,5 +0,0 @@
-import DropDown from './components/navigation/DropDown';
-declare const _default: {
-    DropDown: typeof DropDown;
-};
-export default _default;
--- build/mock/mockImg.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const vertical = "https://interactive-examples.mdn.mozilla.net/media/examples/plumeria.jpg";
-export declare const horizontal = "https://as1.ftcdn.net/jpg/02/44/83/32/500_F_244833214_bBmRijbyEmtKrm7Q5zdcMc4ks3tpTmVu.jpg";
